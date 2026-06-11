57b412d297e2063fa38b02fb1689a423ed21ab00
jpcordebello <cordebello.jp@gmail.com>
2026-06-10 21:25:10 -0300

    Add footer

---
+++ trabalho-grupo4/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer(){
+    return(
+        <footer className="footer">
+            <p>
+                Diário de Avistamentos ET 2026
+            </p>
+            <p>
+                Trabalho em grupo - React + Vite | Serratec
+            </p>
+            <p>
+                Desenvolvido por Bruno Passamani, Lucas Cardoso, João Paulo e Lucas  Lira
+            </p>
+
+        </footer>
+    );
+
+}
+
+export default Footer;
